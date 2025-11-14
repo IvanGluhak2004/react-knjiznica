6495e4120cae8b76cae9f2958e8727efe077f7ab
igluhak <ivan.gluhak@moodmedia.hr>
2025-11-14 12:35:21 +0100

    push na amazonebook

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
+export const books = [
+  {
+    author: "Dav Pilkey",
+    title: "Dog Man",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Dav Pilkey",
+    title: "Cat Kid Comic Club",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "J.K. Rowling",
+    title: "Harry Potter and the Sorcerer's Stone",
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,59 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { books } from "./books";
+import Book from "./book";
 import "./index.css";
-
-const author = "Zdenko";
-const title = "Dog Man";
-const img =
-  "https://m.media-amazon.com/images/I/51UmNg33hCL._UY266_FMwebp_.jpg?aicid=books-design-system-web";
-
-const someFunc = (parm1, parm2) => {
-  console.log(parm1, parm2);
-};
-someFunc("job", "developer");
-
-const FirstBook = {
-  author: author,
-  title: title,
-  img: img,
-};
-const SecondBook = {
-  author: "Dav Pilkey",
-  title: "Cat Kid Comic Club",
-  img: img,
-};
 
 const BookList = () => {
   return (
-    <section className="BookList">
-      <Book
-        img={FirstBook.img}
-        title={FirstBook.title}
-        author={FirstBook.author}
-      >
-        <p>Nesto napisano bilo sto </p>
-        <button>Click me</button>
-      </Book>
-      <Book
-        img={SecondBook.img}
-        title={SecondBook.title}
-        author={SecondBook.author}
-      />
-    </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author, children } = props;
-  console.log(props);
-  return (
-    <article className="Book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
-      {children}
-    </article>
+    <>
+      <h1>Book List</h1>
+      <section className="BookList">
+        {books.map((book, index) => (
+          <Book {...book} key={book.id} number={index} />
+        ))}
+      </section>
+    </>
   );
 };
 
